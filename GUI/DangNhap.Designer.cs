9e9b383ac5c9a3cfd2a1c9480a5cbe6f54e6d637
Trần Gia Bảo <116176422+BaoHero@users.noreply.github.com>
2024-05-18 13:00:07 +0700

    Add files via upload

---
+++ GUI/DangNhap.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace QuanLyBienLai
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDN = new System.Windows.Forms.Button();
+            this.btnTH = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Azure;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 213);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài khoản";
+            // 
+            // txtTK
+            // 
+            this.txtTK.BackColor = System.Drawing.Color.Aquamarine;
+            this.txtTK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTK.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTK.ForeColor = System.Drawing.Color.Black;
+            this.txtTK.Location = new System.Drawing.Point(175, 212);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(350, 38);
+            this.txtTK.TabIndex = 1;
+            // 
+            // txtMK
+            // 
+            this.txtMK.BackColor = System.Drawing.Color.Aquamarine;
+            this.txtMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMK.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.ForeColor = System.Drawing.Color.Black;
+            this.txtMK.Location = new System.Drawing.Point(176, 297);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(350, 38);
+            this.txtMK.TabIndex = 4;
+            this.txtMK.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Azure;
+            this.label2.Font = new System.Drawing.Font("Book Antiqua", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 300);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // btnDN
+            // 
+            this.btnDN.BackColor = System.Drawing.Color.Aqua;
+            this.btnDN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDN.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDN.ForeColor = System.Drawing.Color.Black;
+            this.btnDN.Location = new System.Drawing.Point(225, 433);
+            this.btnDN.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDN.Name = "btnDN";
+            this.btnDN.Size = new System.Drawing.Size(139, 44);
+            this.btnDN.TabIndex = 2;
+            this.btnDN.Text = "Đăng nhập";
+            this.btnDN.UseVisualStyleBackColor = false;
+            this.btnDN.Click += new System.EventHandler(this.btnDN_Click);
+            // 
+            // btnTH
+            // 
+            this.btnTH.BackColor = System.Drawing.Color.LightGray;
+            this.btnTH.BackgroundImage = global::QuanLyBienLai.Properties.Resources.exit;
+            this.btnTH.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTH.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTH.Location = new System.Drawing.Point(525, 0);
+            this.btnTH.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTH.Name = "btnTH";
+            this.btnTH.Size = new System.Drawing.Size(45, 39);
+            this.btnTH.TabIndex = 5;
+            this.btnTH.UseVisualStyleBackColor = false;
+            this.btnTH.Click += new System.EventHandler(this.btnTH_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::QuanLyBienLai.Properties.Resources.HIO1NSI;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(225, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 127);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DangNhap
+            // 
+            this.AcceptButton = this.btnDN;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(569, 519);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnTH);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDN);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.Button btnDN;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTH;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ GUI/DangNhap.cs
@@ -0,0 +1,49 @@
+﻿using QuanLyBienLai.LoginTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace QuanLyBienLai
+{
+    public partial class DangNhap : Form
+    {
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDN_Click(object sender, EventArgs e)
+        {
+            QueriesTableAdapter dn = new QueriesTableAdapter();
+            if(dn.checkLog(txtTK.Text,txtMK.Text) == 1)
+            {
+                main m = new main();
+                m.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng");
+                txtTK.ResetText();
+                txtMK.ResetText();
+            }
+
+            main newtext = new main();
+            newtext.SetLabelText(txtTK.Text);
+            newtext.Show();
+
+        }
+
+        private void btnTH_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
